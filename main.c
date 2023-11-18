053428b4810c7b250e2fa380147a75a0fdd52bc4
paixaoao <aptm@cesar.school>
2023-11-18 10:50:04 -0300

    declarar variavies

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(){
-    int teste;
+// Defina os tamanhos dos arrays conforme necessário
+    int num_processos = 5; // Por exemplo, 5 processos
+    int num_recursos = 3;  // Por exemplo, 3 tipos de recursos
 
+    // Declare os arrays com tamanhos apropriados
+    int alocacao[num_processos][num_recursos];
+    int maximo[num_processos][num_recursos];
+    int necessidade[num_processos][num_recursos];
+    int disponibilidade[num_recursos];
 
+    // Resto do seu código...
 
     return 0;
 }
