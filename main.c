c5b21bb006d42d0e9bee1c7994a96096eb958c7b
paixaoao <aptm@cesar.school>
2023-11-19 14:56:58 -0300

    ler quantidade de processos e recursos

--- main.c
+++ main.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+void alocacaorecurso(alocacao,maximo,necessidade,disponibilidade);
+int request_resources(int customer_num, int request[]);
+void release_resources(int customer_num, int release[]);
 
+int main(int argc, char *argv[])
+{
+     // Verifica se foram fornecidos valores suficientes
+    if (argc < 2) {
+        printf("No resourses.\n");
+        return 1;
+    }
 
-int main(){
-// Defina os tamanhos dos arrays conforme necessário
-    int num_processos = 5; // Por exemplo, 5 processos
-    int num_recursos = 3;  // Por exemplo, 3 tipos de recursos
+    // Calcula a quantidade de valores fornecidos
+    int resources = argc - 1;
+
+    // Aloca espaço para o vetor de processos
+    int *process = (int *)malloc(resources * sizeof(int));
+
+    // Preenche o vetor de valores com os argumentos fornecidos
+    for (int i = 0; i < resources; i++) {
+        process[i] = atoi(argv[i + 1]);
+    }
+
+    // Cria um vetor para armazenar o número de valores
+    int *Resources = (int *)malloc(sizeof(int));
+    *Resources = resources;
+
+    // Imprime a quantidade de valores
+    printf("Quantidade de valores: %d\n", *Resources);
+
+    // Imprime os valores armazenados no vetor
+    printf("Valores: ");
+    for (int i = 0; i < resources; i++) {
+        printf("%d ", process[i]);
+    }
+    printf("\n");
+
+    // Libera a memória alocada para os vetores
+    free(process);
+    free(Resources);
 
     // Declare os arrays com tamanhos apropriados
-    int alocacao[num_processos][num_recursos];
+    /*int available[num_processos][num_recursos];
     int maximo[num_processos][num_recursos];
     int necessidade[num_processos][num_recursos];
-    int disponibilidade[num_recursos];
+    int disponibilidade[num_recursos];*/
 
     // Resto do seu código...
-
     return 0;
 }
